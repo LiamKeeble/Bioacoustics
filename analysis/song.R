1b74138704afcc6ea7995f96a8b10b7528bf5c32
Liam Keeble <liamkeeble@gmail.com>
2022-07-17 23:56:46 +0100

    birdsong

--- analysis/song.R
+++ analysis/song.R
@@ -48,6 +48,40 @@ fit=stan(model_code=mod, data=data)
 print(fit)
 plot(fit)
 
+#Continuous version
+d=data.frame(
+unit=rnorm(20,0,1),
+time=1:20
+)
+
+alpha=matrix(c(0.2,0.3,0.5,0.7), ncol=2, nrow=2)
+
+
+
+mod='
+data{
+vector[20] unit;
+}
+parameters{
+matrix<lower=0,upper=1>[2,2] alpha;
+real<lower=0,upper=1> theta;
+}
+model{
+theta~beta(5,5);
+for (n in 1:20)
+	target += log_mix(theta,
+	normal_lpdf(unit[n] | alpha[1,1], alpha[1,2]),
+	normal_lpdf(unit[n] | alpha[2,1], alpha[2,2]));
+
+}
+'
+
+data=list(
+unit=d$unit
+)
+
+fit=stan(model_code=mod, data=data)
+print(fit)
 
 
 
